5cd54e6aa680fdfd2a565bd125f95ef674271a8c
ivenarelliyo <evanarya7@gmail.com>
2020-03-10 09:27:50 +0700

    9:27 10/03/2020

---
+++ app/src/main/java/com/defalt/pickerfortime/TimePickerFragment.java
@@ -0,0 +1,37 @@
+package com.defalt.pickerfortime;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import java.util.Calendar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Create a new instance of DatePickerDialog and return it.
+        return new TimePickerDialog(getActivity(), this, hour, minute, false);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        MainActivity activity = (MainActivity) getActivity();
+        activity.processTimePickerResult(hourOfDay, minute);
+    }
+}
